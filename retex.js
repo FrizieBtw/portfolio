2e973a8651f91c746198a7833ef99a73e6222778
Paulo Amaral <pamaralsarai@etu.u-bordeaux.fr>
2023-05-30 13:40:46 +0200

    retex infopany

--- retex.js
+++ retex.js
@@ -11,6 +11,10 @@ const rtxAff = document.querySelector('.retex-aff');
 const rtxFen = document.querySelectorAll('.retex-fen');
 const rtx = document.querySelectorAll('.retex');
 const filtre = document.querySelectorAll('.filtre');
+const wrapperOne = document.querySelector('.wrapper-1');
+const leftButton = document.querySelector('.carousel-left');
+const rightButton = document.querySelector('.carousel-right');
+const imageContainer = document.querySelector('.rtx-image-container');
 var openFen;
 var leFiltre;
 
@@ -69,4 +73,32 @@ blogue.addEventListener('click', () => {
 infopany.addEventListener('click', () => {
     overlay(rtxInfopany)
     openFen = rtxInfopany
+});
+
+let track = 0;
+
+let counter = 1;
+
+const moveImagesLeft = function () {
+    if (counter < imageContainer.childElementCount) {
+        counter++;
+        track = track - 100;
+        wrapperOne.style.marginLeft = `${track}%`;
+    }
+}
+
+const moveImagesRight = function () {
+    if (counter > 1) {
+        counter--;
+        track = track + 100;
+        wrapperOne.style.marginLeft = `${track}%`;
+    }
+}
+
+rightButton.addEventListener('click', () => {
+    moveImagesLeft();
+});
+
+leftButton.addEventListener('click', () => {
+    moveImagesRight();
 });
